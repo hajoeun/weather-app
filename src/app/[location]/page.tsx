e7b4ccb8f4bd3d2843cc3fca87a76ff1e007c750
Joeun <hajoeun90@gmail.com>
2023-09-10 17:33:48 +0900

    메타데이터 다루기

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -5,10 +5,20 @@ type Props = {
   params: {
     location: string
   }
+  searchParams: {
+    name: string
+  }
 }
 
-export default async function Detail({ params }: Props) {
-  const name = params.location === 'seoul' ? '서울' : ''
+export function generateMetadata({ searchParams }: Props) {
+  return {
+    title: `날씨 앱 - ${searchParams.name}`,
+    description: `${searchParams.name} 날씨를 알려드립니다`,
+  }
+}
+
+export default async function Detail({ params, searchParams }: Props) {
+  const name = searchParams.name
   const res = await getForecast(params.location)
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default async function Home() {
       <h3>{time.dateTime}</h3>
       <ul className={style.list}>
         <li>
-          <Link href="/seoul">서울</Link>
+          <Link href="/seoul?name=서울">서울</Link>
           <span>{res.current.condition.text}</span>
         </li>
         <li>
-          <Link href="/newyork">뉴욕</Link>
+          <Link href="/NYC?name=뉴욕">뉴욕</Link>
         </li>
         <li>
-          <Link href="/london">런던</Link>
+          <Link href="/london?name=런던">런던</Link>
         </li>
       </ul>
       <RevalidateButton tag={'time'} />
