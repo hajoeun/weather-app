f7732dad27a37a95d8c7b209737e88f953326f97
Joeun <hajoeun90@gmail.com>
2023-09-10 16:00:40 +0900

    날씨 데이터 조회하기 (1) API 사용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@ import Link from 'next/link'
 
 import style from './style.module.css'
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY + '111'
+
+const getCurrentWeather = async () => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`
+  )
+
+  if (!res.ok) {
+    if (!res.ok) throw new Error('날씨 정보를 가져올 수 없습니다.')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const res = await getCurrentWeather()
+  console.log(res)
+
   return (
     <>
       <h1>날씨 앱</h1>
