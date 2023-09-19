36a38e26a49c16922eabf68a34b12cf117c33805
hajoeun <hajoeun90@gmail.com>
2023-09-20 08:59:24 +0900

    홈에 CityWeatherText 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,32 @@
 import Link from 'next/link'
 
-import style from './style.module.css'
-import { getCurrentWeather } from '@/utils/getCurrentWeather'
-import { getTime } from '@/utils/getTime'
 import RevalidateButton from '@/components/RevalidateButton'
+import CityWeatherText from '@/components/CityWeatherText'
+
+import style from './style.module.css'
 
 export default async function Home() {
-  const res = await getCurrentWeather('seoul')
-  const time = await getTime(res.location.tz_id)
+  const cities = [
+    { name: '서울', code: 'seoul' },
+    { name: '뉴욕', code: 'NYC' },
+    { name: '런던', code: 'london' },
+  ]
 
   return (
     <>
       <h1>날씨 앱</h1>
-      <h3>{time.dateTime}</h3>
+      <RevalidateButton tag="time" />
       <ul className={style.list}>
-        <li>
-          <Link href="/seoul?name=서울">서울</Link>
-          <span>{res.current.condition.text}</span>
-        </li>
-        <li>
-          <Link href="/NYC?name=뉴욕">뉴욕</Link>
-        </li>
-        <li>
-          <Link href="/london?name=런던">런던</Link>
-        </li>
+        {cities.map((city) => {
+          const path = `/${city.code}?name=${city.name}`
+          return (
+            <li key={city.name}>
+              {city.name} / <CityWeatherText cityCode={city.code} />(
+              <Link href={path}>예보 확인</Link>)
+            </li>
+          )
+        })}
       </ul>
-      <RevalidateButton tag={'time'} />
     </>
   )
 }
