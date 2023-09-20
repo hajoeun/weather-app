cde90138aee8dfe1f9df31330b6894cdebd509d1
hajoeun <hajoeun90@gmail.com>
2023-09-20 21:45:22 +0900

    상세에 컴포넌트 적용
    - ForecastItem
    - CurrentWeatherCondition
    - RevalidateButton

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,5 +1,10 @@
+import CurrentWeatherCondition from '@/components/CurrentWeatherCondition'
+import ForecastItem from '@/components/ForecastItem'
 import HomeButton from '@/components/HomeButton'
+import RevalidateButton from '@/components/RevalidateButton'
 import { getForecast } from '@/utils/getForecast'
+
+import style from './style.module.css'
 
 type Props = {
   params: {
@@ -19,19 +24,34 @@ export function generateMetadata({ searchParams }: Props) {
 
 export default async function Detail({ params, searchParams }: Props) {
   const name = searchParams.name
-  const res = await getForecast(params.location)
+  const { location, current, forecast } = await getForecast(params.location)
 
   return (
     <>
       <h1>{name}의 3일 예보</h1>
+      <div className={style.buttonGroup}>
+        <HomeButton />
+        <RevalidateButton tag="time" />
+      </div>
+      <div className={style.header}>
+        <span>현재 날씨 -</span>{' '}
+        <CurrentWeatherCondition
+          timeZone={location.tz_id}
+          conditionText={current.condition.text}
+          conditionIcon={current.condition.icon}
+        />
+      </div>
       <ul>
-        {res.forecast.forecastday.map((day) => (
-          <li key={day.date}>
-            {day.date} / {day.day.avgtemp_c}
-          </li>
+        {forecast.forecastday.map((day) => (
+          <ForecastItem
+            key={day.date}
+            date={day.date}
+            temperature={day.day.avgtemp_c}
+            conditionText={day.day.condition.text}
+            conditionIcon={day.day.condition.icon}
+          />
         ))}
       </ul>
-      <HomeButton />
     </>
   )
 }
